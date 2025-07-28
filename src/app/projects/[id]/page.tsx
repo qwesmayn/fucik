8097ac6c3ea4cf3c7a03f262a6de29389f856648
qwesmayn <109741981+qwesmayn@users.noreply.github.com>
2025-07-28 19:59:45 +0300

    Refactor ProjectPage component to handle async params for improved data fetching

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,3 +1,9 @@
-export default function ProjectPage({ params }: { params: { id: string } }) {
-  return <div>Project {params.id}</div>;
+export default async function ProjectPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return <div>Project {id}</div>;
 }
